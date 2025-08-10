67784c5cb4429b7d61f79e37e2324f9ef75d0ebe
dani383828 <tubedanial@gmail.com>
2025-08-10 11:33:04 +0330

    Update photo_manager.py

--- photo_manager.py
+++ photo_manager.py
@@ -1,4 +1,3 @@
 # photo_manager.py
 def get_invite_photo():
-    # file_id عکس ثابت برای بخش دعوت
-    return "///////"  # اینو با file_id واقعی عکس جایگزین کن
+    return "AgACAgQAAxkBAAIMwmiYUbn7-DTF7DzDbhpjs_YqLss7AALRyTEbfnnBUBqmkwho8a4MAQADAgADeQADNgQ"
